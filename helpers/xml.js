79f136e5f1a182bc35df52c98f4b16d0d2adcb1a
Sergio Fiorotti <s.fiorotti@hotmail.com>
2021-12-20 19:09:55 -0300

    fix: xml parser

--- helpers/xml.js
+++ helpers/xml.js
@@ -1,19 +1,19 @@
 exports.toXml = function (json) {
-    const Parser = require("fast-xml-parser").j2xParser
+    const Parser = require('fast-xml-parser').j2xParser
     //default options need not to set
     const defaultOptions = {
-        attributeNamePrefix : "@_",
-        attrNodeName: "@", //default is false
-        textNodeName : "#text",
+        attributeNamePrefix : '@_',
+        attrNodeName: '@', //default is false
+        textNodeName : '#text',
         ignoreAttributes : true,
-        cdataTagName: "__cdata", //default is false
-        cdataPositionChar: "\\c",
+        cdataTagName: '__cdata', //default is false
+        cdataPositionChar: '\\c',
         format: true,
-        indentBy: "  ",
+        indentBy: '  ',
         suppressEmptyNode: false,
         // tagValueProcessor: a=> he.encode(a, { useNamedReferences: true}),// default is a=>a
         // attrValueProcessor: a=> he.encode(a, {isAttributeValue: isAttribute, useNamedReferences: true}),// default is a=>a
-        rootNodeName: "element"
+        rootNodeName: 'element'
     }
     const parser = new Parser(defaultOptions)
     return parser.parse(json)
