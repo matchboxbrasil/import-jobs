c1d02414c88571240fae8df316dc87131c0c3b7e
Sergio Fiorotti <s.fiorotti@hotmail.com>
2021-12-20 23:09:27 -0300

    fix: remove comments

--- helpers/xml.js
+++ helpers/xml.js
@@ -1,18 +1,15 @@
 exports.toXml = function (json) {
     const Parser = require('fast-xml-parser').j2xParser
-    //default options need not to set
     const defaultOptions = {
         attributeNamePrefix : '@_',
-        attrNodeName: '@', //default is false
+        attrNodeName: '@',
         textNodeName : '#text',
         ignoreAttributes : true,
-        cdataTagName: '__cdata', //default is false
+        cdataTagName: '__cdata',
         cdataPositionChar: '\\c',
         format: false,
         indentBy: '  ',
         suppressEmptyNode: false,
-        // tagValueProcessor: a=> he.encode(a, { useNamedReferences: true}),// default is a=>a
-        // attrValueProcessor: a=> he.encode(a, {isAttributeValue: isAttribute, useNamedReferences: true}),// default is a=>a
         rootNodeName: 'element'
     }
     const parser = new Parser(defaultOptions)
